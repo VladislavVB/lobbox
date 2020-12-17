411a254eef643a095684e8aa04dc72c44851f096
VladislavVB <burdukovskij.vlad@mail.ru>
2020-12-17 20:26:09 +0300

    fix faq

--- src/js/main.js
+++ src/js/main.js
@@ -24,68 +24,26 @@ let closeMenu = () => {
 }
 
 //плюс текст
-let openText1 = () => {
-    document.querySelector('.faq-card-text-1').classList.add('faq-card-text-active-1');
+let openText = (arg) => {
+    // document.querySelector('#' + arg).classList.add('faq-card-text-active');
+    let elem = document.querySelector('#' + arg);
+    if (elem.classList.contains('faq-card-text-active')) {
+        elem.classList.remove('faq-card-text-active')
+        document.querySelector('.faq-plus-' + arg).src = 'img/footer/Plusbutton.png'
+       
+    } else {
+        document.querySelector('.faq-plus-' + arg).src = 'img/footer/PlusbuttonBg.png'
+        elem.classList.add('faq-card-text-active')
+    }
 }
-let closeText1 = () => {
-    document.querySelector('.faq-card-text-1').classList.remove('faq-card-text-active-1');
-}
+// console.log(document.querySelector('faq-plus-first'));
+
+// let closeText = (arg) => {
+//     document.querySelector('#' + arg).classList.remove('faq-card-text-active');
+// }
 
 
-let openText2 = () => {
-    document.querySelector('.faq-card-text-2').classList.add('faq-card-text-active-2');
-}
-let closeText2 = () => {
-    document.querySelector('.faq-card-text-2').classList.remove('faq-card-text-active-2');
-}
 
-
-let openText3 = () => {
-    document.querySelector('.faq-card-text-3').classList.add('faq-card-text-active-3');
-}
-let closeText3 = () => {
-    document.querySelector('.faq-card-text-3').classList.remove('faq-card-text-active-3');
-}
-
-
-let openText4 = () => {
-    document.querySelector('.faq-card-text-4').classList.add('faq-card-text-active-4');
-}
-let closeText4 = () => {
-    document.querySelector('.faq-card-text-4').classList.remove('faq-card-text-active-4');
-}
-
-
-let openText5 = () => {
-    document.querySelector('.faq-card-text-5').classList.add('faq-card-text-active-5');
-}
-let closeText5 = () => {
-    document.querySelector('.faq-card-text-5').classList.remove('faq-card-text-active-5');
-}
-
-
-let openText6 = () => {
-    document.querySelector('.faq-card-text-6').classList.add('faq-card-text-active-6');
-}
-let closeText6 = () => {
-    document.querySelector('.faq-card-text-6').classList.remove('faq-card-text-active-6');
-}
-
-
-let openText7 = () => {
-    document.querySelector('.faq-card-text-7').classList.add('faq-card-text-active-7');
-}
-let closeText7 = () => {
-    document.querySelector('.faq-card-text-7').classList.remove('faq-card-text-active-7');
-}
-
-
-let openText8 = () => {
-    document.querySelector('.faq-card-text-8').classList.add('faq-card-text-active-8');
-}
-let closeText8 = () => {
-    document.querySelector('.faq-card-text-8').classList.remove('faq-card-text-active-8');
-}
 
 //закртые снизу
 let closeFooter = () => {
